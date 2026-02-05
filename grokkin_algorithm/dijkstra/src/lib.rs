67375f582c211fe06383e1edc5a2b0f4ff0bba7b
Yoshishige Unose <966.krr@gmail.com>
2026-02-05 14:34:06 +0900

    20260205 update

--- grokkin_algorithm/dijkstra/src/lib.rs
+++ grokkin_algorithm/dijkstra/src/lib.rs
@@ -1,96 +1,71 @@
-#![warn(clippy::pedantic)]
+// Data Types for the project
+// Input Graph:
+// Graph = HashMap<&str, HashMap<&str, u32>>
+//
+// Cost Container:
+// HashMap<String, u32>
+//
+// Parent Container:
+// HashMap<String, String>
+// 
+type Graph = HashMap<&str, HashMap<&str, u32>>;
 
-use std::collections::HashMap;
+struct ParentChildPair {
+    parent: String,
+    children: HashMap<&str, u32>,
+}
 
-type Graph<'a> = HashMap<&'a str, HashMap<&'a str, u32>>;
+impl for ParentChildPair {
+    fn new(&graph) -> ParentChildPair {
 
-fn run(base_graph: &Graph) {
-    let mut cost_map: HashMap<&str, u32> = HashMap::new();
-    let mut _path_map: HashMap<&str, &str> = HashMap::new();
-
-    for (node, child_node) in base_graph {
-        println!("in first for loop");
-        println!("{node:?}");
-        update_cost(&node, &child_node, &mut cost_map);
-
-        // update_path(&node, &child_node, &path);
-
-        // if I call update_path() in update_cheapest, I would have to pass
-        // 'path' here. Which somehow, makes me think that it's not gonna
-        // have a beautiful function signature... If I were to have an
-        // independent function of parent node, update cheaptest node must
-        // pass a flag that update_path should be invoked as well.
     }
 }
 
-fn update_cost(node: &str, child_node: &HashMap<&str, u32>, cost_map: &HashMap<&str, u32>) {
-    if let Some(cost_of_processing_node) = cost_map.get(node) {
-        println!("{cost_of_processing_node}");
-    };
+// runs the logic of algorithm
+fn run(graph: &Graph) -> String {
+    // container declaration
+    let cost_container: HashMap<String, u32> = HashMap::new();
+    let parent_container: HashMap<String, String> = HashMap::new();
 
-    for (key, value) in child_node {
-        println!("In update_cost function!");
-        println!("key: {key}");
-        println!("value: {value}");
-    }
-    // get the cost of processing node to calculate the total cost when
-    // comparing with the cost in map
-    /*
-    let Some(process_node_cost) = cost_map.get(process_node) else {}
+    // get the processing node from the Graph and its child nodes
+    // the start of loop
+    for (process_node, child_nodes) in graph {
+        // create the data structure to pass around
+        parent_child = ParentChildPair::new();
 
-    for child_node in node.keys() {
-        child_node.get
-        if Ok(cheapest_cost) = cost_map.get(child_node) {
-
-            if cost +  cost_map.get(key)
-
-        }
-
-        match {
-            Some(cost) if cheapest_cost > cost + process_node_cost {
-
-            }
+        // update the costs in cost_container
+        if let Ok(better_pair: Vec<CheapPair>) = update_cost(child_nodes, cost_container) {
+            // updates parent if Option<CheapPair> is returned from update_cost
+            update_parent(better_pair, parent_container);
         }
     }
-    */
+    // repeat the above flow until there is no more node to process
+    // repeats on processing node
+    
+    // format the result (in String type)
+    let result = format(cost_container, parent_container);
+
+    // return result
+    result
 }
 
-fn key_is_empty(cost_map: &mut HashMap<&str, u32>, map_to_check: &HashMap<&str, u32>) {
-    let current_cost = map_to_check["A"];
-    cost_map
-        .entry("A")
-        .and_modify(|cost| {
-            *cost = if *cost > current_cost + 3 {
-                current_cost + 3
-            } else {
-                *cost
-            };
-        })
-        .or_insert(current_cost);
+fn update_cost(
+    child_nodes: HashMap<&str, u32>,
+    cost_container: HashMap<String, u32>
+    ) -> Option<Vec<CheapPair>> {
+    // check the container if it needs an update 
+    // update when:
+    // 1. the container is empty
+    // 2. the container and the processing node's child is the same
+    //    AND the cost to the processing node + the cost to its child node is smaller
+    //    than the cost of the node in the container that's being compared
+
+    // if updated, store every key of the child nodes and the processing node (the parent)
+    // how the heck am I going to store the processing node...
+
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_run() {
-        let base_graph = HashMap::from([
-            ("Start", HashMap::from([("A", 6), ("B", 2)])),
-            ("A", HashMap::from([("Finish", 1)])),
-            ("B", HashMap::from([("A", 3), ("Finish", 5)])),
-            ("Finish", HashMap::new()),
-        ]);
-
-        run(&base_graph);
-    }
-
-    #[test]
-    fn check_key() {
-        let mut cost_map: HashMap<&str, u32> = HashMap::from([("A", 25)]);
-        let map_to_check = HashMap::from([("A", 17)]);
-        key_is_empty(&mut cost_map, &map_to_check);
-
-        assert_eq!(17 + 3, cost_map["A"]);
-    }
-}
+fn update_parent() {
+    // use Vec<CheapPair> to iterate over the pair in cost_container
+    // ...now, this is a bad logic!! Fix it next time.
+} 
