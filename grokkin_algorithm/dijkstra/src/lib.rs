4d415e55571189a5b5822beda2da1325ef226101
Yoshishige Unose <966.krr@gmail.com>
2026-02-20 14:22:51 +0900

    add test that checks the entry of cost_table

--- grokkin_algorithm/dijkstra/src/lib.rs
+++ grokkin_algorithm/dijkstra/src/lib.rs
@@ -13,7 +13,7 @@ struct CheapestRoute {
 }
 
 impl CheapestRoute {
-    fn new () -> Self {
+    fn new () -> CheapestRoute {
         Self {
             cost: HashMap::new(), // does this need to be a HashMap?
             parent: HashMap::new(),
@@ -21,8 +21,24 @@ impl CheapestRoute {
     }
 
     // registers the cost if the path through the passed node is the cheapest
-    pub fn register_cost(node: HashMap<&str, u32>) {
-        todo!();
+    pub fn register_cost(&mut self, node: &HashMap<&str, u32>) {
+        // check entries of the table
+        if self.cost.is_empty() {
+            for (out_neighbor, cost) in node {
+                self.cost.insert(out_neighbor.to_string(), *cost);
+            }
+        }
+
+        // no entry => insert the cost and return
+        // has entry => 
+            // 1. add the cost to the existing entry and the cost to current
+            //    processing node
+            // 2. compare the result from step 1 and the existing entry
+            // 3. step 2::equal => do nothing,
+            //    step 2::result is greater => do nothing,
+            //    step 2::result is smaller => replace the value with the result
+
+        // repeat as many out neighbors
     }
 
     // tracks the route of the cheapest cost 
@@ -40,17 +56,24 @@ impl CheapestRoute {
 
 fn run(graph: &Graph) -> String {
     let keys: Vec<&str> = graph.keys().map(|key| *key).collect();
-    let cheapest_route = RoutingTable::new();
+    let cheapest_route = CheapestRoute::new();
 
     let (processing_key, _finish_key) = get_starting_key(graph);
 
     todo!()
-
+/*
     // operate until there's no more key to process
     while !keys.is_empty() {
         // get the node to process
+        let processing_node = graph.get(processing_key).unwrap();
 
         // try registering the costs 
+
+        // pop the processing key from keys list
+        keys.pop(processing_key); // will this even work...??
+
+        // assign the next processing key
+        processing_key =  
 
     }
 
@@ -59,7 +82,19 @@ fn run(graph: &Graph) -> String {
 
     // return a String data containing the information of the cheapest paths
     result
+*/
+}
 
+// gets the key for next process
+fn next_key(node: HashMap<&str, u32>) -> String {
+    todo!()
+    // get the costs of all out neighbors
+
+    // return the cost if only one out negihbor exist
+
+    // find the cheapest from the list
+
+    // return the cheapest
 }
 
 fn get_starting_key<'a>(graph: &'a Graph<'a>) -> (&'a str, &'a str) {
@@ -106,8 +141,8 @@ mod tests {
 
     #[test]
     fn new_table() {
-        let cheapest_route = RoutingTable::new();
-        assert!(cheapest_route.cheapest_cost.is_empty());
+        let cheapest_route = CheapestRoute::new();
+        assert!(cheapest_route.cost.is_empty());
     }
 
     #[test]
@@ -122,5 +157,21 @@ mod tests {
         let (starting_key, finish_key) = get_starting_key(&graph);
         assert_eq!("start", starting_key);
         assert_eq!("finish", finish_key);
+    }
+    #[test]
+    fn register_cost() {
+       let test_input = HashMap::from([
+           ("a", 3_u32),
+           ("b", 4_u32)
+       ]);
+       let mut cheapest_route = CheapestRoute::new();
+
+       cheapest_route.register_cost(&test_input);
+
+       let test_output = HashMap::from([
+           ("a".to_string(), 3_u32),
+           ("b".to_string(), 4_u32)
+       ]);
+       assert_eq!(test_output, cheapest_route.cost);
     }
 }
