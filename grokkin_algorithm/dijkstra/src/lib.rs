871def743dd1b10acfa5d52d48a6af91f0d385f8
Yoshishige Unose <966.krr@gmail.com>
2026-03-13 10:23:17 +0900

    update

--- grokkin_algorithm/dijkstra/src/lib.rs
+++ grokkin_algorithm/dijkstra/src/lib.rs
@@ -1,28 +1,76 @@
-use std::collections::HashMap;
+// Calculating the cheapest path in a network given the source node and
+// destination node
+//
+// input: network information, source node and destination node
+// output: 1 the cost of cheapest path, 2 the route of the cheapest path
+//
+// constraint: only use HashMap to store nodes and cost, use dijkstra algorithm
+//
+// dijkstra algorithm:
+// 1. look for neighbor nodes from the source node
+// 2. find the neighbor which is cheapest to go to
+// 3. do step 1 and 2 with the neighbor you went
+// 4. repeat until you finish examining from every node
+//
+// data:
+// 1. to keep track if it went through all the nodes Vec<String>
+// 2. to store the information of cheapest route <HashMap<child, parent>>
+// 3. to sotre the cost of 2 <u32>
+use std::collections::{HashMap, HashSet};
 
-pub struct Route {
-    start: String,
-    finish: String,
+fn run(network: &Network, source: &str, destination: &str) -> String {
+    // get all the nodes in the Network
+
+    // *** Processor tasks
+    // check the neighbors of source node
+    //
+    // store their information
+    // *** Processor tasks
+    //
+    // move to the cheapest out of all neighbors
+    //
+    // repeat untill every node is processed
+
+    // format the output
 }
 
-impl Route {
-    pub fn from_to(start: String, finish: String) -> Self {
-        Route { start, finish }
+// compare with the list of input nodes and return true if it has
+fn has_visited_all_nodes(network_nodes: &[&str], processed_nodes: &[&str]) -> bool {
+    let network = network_nodes.iter().collect::<HashSet<_>>();
+    let processed = processed_nodes.iter().collect::<HashSet<_>>();
+
+    network == processed
+    /*
+    let mut result = false;
+    // don't even compare when len() does not equal
+    if network_nodes.len() == processed_nodes.len() {
+        // check if two list have identical content
+        for node in processed_nodes {
+            if !network_nodes.contains(node) {
+                println!("something went wrong");
+                return false;
+            }
+        }
+        result = true;
     }
-
-    pub fn cheapest_route(network: Network) -> String {}
+    result
+    */
 }
 
-pub struct Network(HashMap<String, HashMap<String, u32>>);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-impl Network {
-    fn get_all_nodes(&self) -> Vec<String> {}
+    #[test]
+    fn visited_all_nodes() {
+        // case 1: first loop (only the source)
+        // case 2: in middle of the loop (source and others)
+        // case 3: processed all nodes (returns true)
+        let network_nodes = ["one", "two", "threea"];
+        let processed_nodes = ["two", "one", "threea"];
+
+        let checker = has_visited_all_nodes(&network_nodes, &processed_nodes);
+
+        assert!(checker);
+    }
 }
-
-struct Node(HashMap<String, u32>);
-
-impl Node {
-    fn cheapest_neighbor(&self) -> String {}
-}
-// where the heck will the
-// ...how about, .cheapest_neighbor() ?
--- grokkin_algorithm/dijkstra/src/main.rs
+++ grokkin_algorithm/dijkstra/src/main.rs
@@ -1,40 +1,21 @@
 /// below are *Translation* of Dijkstra Algorithm's
 /// pseudo code in Grokking Algorithm
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::DebugTuple};
 
-type Graph = HashMap<&str, HashMap<&str, u32>>;
+type Network = HashMap<&str, HashMap<&str, u32>>;
 
 fn main() {
-    // create graph
-    let graph: Graph = HashMap::from([
-        ("start", HashMap::from([("a", 6), ("b", 2)])),
-        ("a", HashMap::from([("finish", 1)])),
-        ("b", HashMap::from([("a", 3), ("finish", 5)])),
-        ("finish", HashMap::new())
+    let source = "start";
+    let destination = "finish";
+
+    // create Network
+    let network: Network = HashMap::from([
+        (source, HashMap::from([("a", 6), ("b", 2)])),
+        ("a", HashMap::from([(destination, 1)])),
+        ("b", HashMap::from([("a", 3), (destination, 5)])),
+        (destination, HashMap::new()),
     ]);
 
-    // repeat infinity -> I don't know if I can include this in Rust
-    todo!("Do I need to replace infinity = float('inf')??");
-
-    // create a hash table to record the cost from the start
-    // to the current node
-    let mut cost_table = HashMap::from([
-        ("a", 6), 
-        ("b", 2),
-        ("finish", infinity)
-        todo!("Above line needs Rust specific translation");
-    ]);
-
-    // create another hash table to store the parents
-    let mut parent_table = HashMap::from([
-        ("a", "start"),
-        ("b", "start"),
-        ("finish", HashMap::new())
-    ]);
-
-    // create an array to keep track of all the processed nodes
-    // ** this line was what made me to stuck for a month **
-    let mut processed_nodes: Vec<&str> = Vec::new();
-
-    // returns the lowest_
+    let reuslt = run(&network, source, destination);
+    println!("{reuslt}");
 }
